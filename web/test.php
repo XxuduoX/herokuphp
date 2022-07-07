eef640ba44e0ecd8b361b55dc85bfb27f6a4bf5c
XxuduoX <20007998@myrp.edu.sg>
2022-07-07 12:23:51 +0800

    commit message goes here2

--- web/test.php
+++ web/test.php
@@ -1,7 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
+
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test</title>
 </head>
+
 <body>
-  Hello World!
+    Hello World!
 </body>
+
 </html>
